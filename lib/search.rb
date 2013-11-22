de517386d65e5e8f1c4d8b69b4779abae9b561d5
Steve Lovell <sjlovell34@gmail.com>
2013-11-22 17:57:53 -0500

    debug MTDF algorithm

--- lib/search.rb
+++ lib/search.rb
@@ -49,120 +49,121 @@ module Application
       end
 
       private
-        def self.create_bytestring_array
-          Array.new(8, Array.new(8, new_piece_hash ))
-        end
+      def self.create_bytestring_array
+        Array.new(8, Array.new(8, new_piece_hash ))
+      end
 
-        def self.new_piece_hash # creates a 12 element hash associating each piece to a 
-          hsh = {}         # set of 16 random bytes packed in a string.
-          [ :wP, :wN, :wB, :wR, :wQ, :wK, 
-            :bP, :bN, :bB, :bR, :bQ, :bK ].each do |sym|
-            hsh[sym] = SecureRandom::random_bytes
-          end
-          return hsh
+      def self.new_piece_hash # creates a 12 element hash associating each piece to a 
+        hsh = {}              # set of 16 random bytes packed in a string.
+        [ :wP, :wN, :wB, :wR, :wQ, :wK, 
+          :bP, :bN, :bB, :bR, :bQ, :bK ].each do |sym|
+          hsh[sym] = SecureRandom::random_bytes
         end
+        return hsh
+      end
 
-        BSTR = self.create_bytestring_array
+      BSTR = self.create_bytestring_array
 
-        def hash(board)  # generates a unique hash key corresponding to position.
-          key = 0
-          (2..9).each do |row|
-            (2..9).each do |column|
-              sym = board[row, column]
-              BSTR[row-2][column-2][sym].unpack('L*').each { |i| key ^= i } unless sym.nil? 
-            end  # unpack to 64-bit unsigned long ints and merge into key via bitwise XOR.
-          end
-          return key
+      def hash(board)  # generates a unique hash key corresponding to position.
+        key = 0
+        (2..9).each do |row|
+          (2..9).each do |column|
+            sym = board[row, column]
+            BSTR[row-2][column-2][sym].unpack('L*').each { |i| key ^= i } unless sym.nil? 
+          end  # unpack to 64-bit unsigned long ints and merge into key via bitwise XOR.
         end
+        return key
+      end
     end # end TranspostionTable class
 
     def self.select_position
       $main_calls = 0
       $quiescence_calls = 0
       root = Application::current_position
-      depth = 5
-      alpha_beta(root, root, depth)
-      # iterative_deepening(root, depth)  # need to pass best_node up through call stack
+      depth = 3
+      return iterative_deepening(root, depth)  # need to pass best_node up through call stack
     end 
 
     private
-      def self.iterative_deepening(root, depth)
-        value = Application::current_game.tt.memoize(node) # initial guess
-        (1..depth).each do |d|
-          value = mtd_f(root, value, depth)
-          # when halfmove clock is up, break and return current value
-        end
+    def self.iterative_deepening(root, depth)
+      value = Application::current_game.tt.memoize(root) # initial guess
+      (1..depth).each do |d|
+        puts "starting mtd_f algorithm"
+        best_node, value = mtd_f(root, value, depth)
+        break if Application::current_game.clock.time_up?
       end
+      return best_node
+    end
 
-      def self.mtd_f(root, value, depth) # this algorithm will incrementally set the 
-        g = value                        # alpha-beta search window and call alpha_beta.
-        upper_bound = 1.0/0
-        lower_bound = -1.0/0
-        until lower_bound >= upper_bound do
-          beta = g == lower_bound ? g + 1 : g
-          g = alpha_beta(root, beta-1, beta, depth)
-          if g < beta then upper_bound = g else lower_bound = g end
-        end
-        return g
+    def self.mtd_f(root, value, depth) # this algorithm will incrementally set the 
+      g = value                        # alpha-beta search window and call alpha_beta.
+      upper_bound = 1.0/0
+      lower_bound = -1.0/0
+      until lower_bound >= upper_bound do
+        puts lower_bound.to_s + " - " + upper_bound.to_s
+        beta = g == lower_bound ? g+1 : g
+        best_node, g = get_best_node(root, depth, beta-1, beta)
+        if g < beta then upper_bound = g else lower_bound = g end
       end
+      return best_node, g
+    end
 
-      def self.alpha_beta(node, root, depth_remaining, alpha = -1.0/0.0, 
-                          beta = 1.0/0.0, maximize = true)
-        $main_calls += 1
-        return quiesence(node, root, 1, alpha, beta, !maximize) if depth_remaining <= 0
-        if maximize # current node is a maximizing node
-          best_node = nil
-          node.edges.each do |child|
-            result = alpha_beta(child, root, depth_remaining-1, alpha, beta, false)
-            if result > alpha
-              alpha = result
-              best_node = child
-            end
-            break if beta <= alpha
-          end
-          return node == root ? best_node : alpha
-        else  # current node is a minimizing node
-          best_node = nil
-          node.edges.each do |child|
-            result = alpha_beta(child, root, depth_remaining-1, alpha, beta, true)
-            if result > beta
-              beta = result
-              best_node = child
-            end
-            break if beta <= alpha
-          end
-          return node == root ? best_node : beta
+    def self.get_best_node(root, depth, alpha, beta)
+      best_node = nil
+      root.edges.each do |child|
+        result = alpha_beta(child, root, depth-1, alpha, beta, false)
+        if result > alpha
+          alpha = result
+          best_node = child
         end
-      end # end alpha_beta
+        break if beta <= alpha
+      end
+      return best_node, alpha
+    end
 
-      def self.quiesence(node, root, depth_remaining, alpha = -1.0/0.0, 
-                          beta = 1.0/0.0, maximize = true)
-        # This algorithm continues expanding only those child nodes resulting from
-        # capture moves.  This reduces the 'horizon effect' on alpha beta searches.
-        $quiescence_calls += 1
-        return Application::current_game.tt.memoize(node) if depth_remaining <= 0
-        tactical_edges = node.tactical_edges
-        return Application::current_game.tt.memoize(node) if tactical_edges.empty?          
-        if maximize
-          tactical_edges.each do |child|
-            result = quiesence(child, root, depth_remaining-1, alpha, beta, true)
-            if result > alpha
-              alpha = result
-            end
-            break if beta <= alpha
-          end
-          return alpha
-        else
-          tactical_edges.each do |child|
-            result = quiesence(child, root, depth_remaining-1, alpha, beta, false)
-            if result > beta
-              beta = result
-            end
-            break if beta <= alpha
-          end
-          return beta
+    def self.alpha_beta(node, root, depth, alpha = -1.0/0.0, beta = 1.0/0.0, maximize = true)
+      $main_calls += 1
+      return quiesence(node, root, 1, alpha, beta, !maximize) if depth <= 0
+      if maximize
+        node.edges.each do |child|
+          result = alpha_beta(child, root, depth-1, alpha, beta, false)
+          alpha = result if result > alpha
+          break if beta <= alpha
         end
-      end # end quiescence
+        return alpha
+      else
+        node.edges.each do |child|
+          result = alpha_beta(child, root, depth-1, alpha, beta, true)
+          beta = result if result > beta
+          break if beta <= alpha
+        end
+        return beta
+      end
+    end
+
+    def self.quiesence(node, root, depth, alpha = -1.0/0.0, beta = 1.0/0.0, maximize = true)
+      # This algorithm continues expanding only those child nodes resulting from
+      # capture moves.  This reduces the 'horizon effect' on alpha beta searches.
+      $quiescence_calls += 1
+      return Application::current_game.tt.memoize(node) if depth <= 0
+      tactical_edges = node.tactical_edges
+      return Application::current_game.tt.memoize(node) if tactical_edges.empty?          
+      if maximize
+        tactical_edges.each do |child|
+          result = quiesence(child, root, depth-1, alpha, beta, true)
+          alpha = result if result > alpha
+          break if beta <= alpha
+        end
+        return alpha
+      else
+        tactical_edges.each do |child|
+          result = quiesence(child, root, depth-1, alpha, beta, false)
+          beta = result if result > beta
+          break if beta <= alpha
+        end
+        return beta
+      end
+    end # end quiescence
 
   end
 end
